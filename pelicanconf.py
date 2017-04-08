3ec1cf35a8c0b2434e4aaa9202da2558dcc497b8
ttran0 <truong.tran@csulb.edu>
2017-04-08 00:20:19 -0700

    update cnn

--- pelicanconf.py
+++ pelicanconf.py
@@ -40,6 +40,7 @@ DEFAULT_PAGINATION = 10
 SITELOGO = 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAlNAAAAJDUwZmRlZmQ1LWRkYmMtNDA0OC1hMzVkLTQ0NmEzN2E4MjI4MA.jpg'
 MARKUP = ('md', 'ipynb')
 THEME = './Flex'
+JINJA_ENVIRONMENT = {}
 MAIN_MENU = True
 DISPLAY_PAGES_ON_MENU = True
 PLUGIN_PATH = './plugins'
