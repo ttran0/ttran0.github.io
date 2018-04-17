483f783ddbed7948486fec634eea2cad1654b3aa
ttran0 <truong.tran@csulb.edu>
2018-04-16 21:09:46 -0700

    updated with picture

--- pelicanconf.py
+++ pelicanconf.py
@@ -37,7 +37,7 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
-SITELOGO = 'https://media.licdn.com/dms/image/C5103AQE-KW5gH0banw/profile-displayphoto-shrink_200_200/0?e=1529121600&v=beta&t=t7WWD9umuS0wPmwZbYJQBMajMvCBZHHWEbQuy67A_1M.jpg'
+SITELOGO = 'https://media.licdn.com/dms/image/C5103AQE-KW5gH0banw/profile-displayphoto-shrink_200_200/0?e=1529121600&v=beta&t=t7WWD9umuS0wPmwZbYJQBMajMvCBZHHWEbQuy67A_1M'
 # SITELOGO = 'sitelogo.png'
 MARKUP = ('md', 'ipynb')
 THEME = './Flex'
